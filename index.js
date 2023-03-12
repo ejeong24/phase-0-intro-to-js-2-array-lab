8cb9de3f5a35101be7988b9611f3d1a3e9be6024
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-03-12 15:59:15 -0400

    testing passed

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield"
+];
+
+function destructivelyAppendCat (catName) {
+cats.push(catName);
+}
+
+function destructivelyPrependCat(catName) {
+    cats.unshift(catName);
+}
+
+function destructivelyRemoveLastCat(catName) {
+    cats.pop(catName);
+}
+
+function destructivelyRemoveFirstCat(catName) {
+    cats.shift(catName);
+}
+
+function appendCat(catName) {
+   const catsCopy = [...cats, catName];
+   return catsCopy;
+}
+
+function prependCat(catName) {
+   const catsCopy = [catName, ...cats];
+   return catsCopy;
+}
+
+function removeLastCat (catName) {
+    const catsCopy = [...cats];
+    return catsCopy.slice(0,-1);
+}
+
+function removeFirstCat (catName) {
+    const catsCopy = [...cats];
+    return catsCopy.slice(1);
+}
